fa836d3c8e84b053cc5856abe8588b7431cbcd41
Ackers <mark@ackroyd.net>
2023-11-16 14:41:45 +0000

    Tidy Up

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -1,23 +1,3 @@
--- -------------------------------------------------------------
--- TablePlus 4.8.2(436)
---
--- https://tableplus.com/
---
--- Database: GoTalk
--- Generation Time: 2023-11-14 16:43:18.7950
--- -------------------------------------------------------------
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 CREATE TABLE `postcodes` (
                              `id` int(11) NOT NULL AUTO_INCREMENT,
                              `postcode` varchar(10) DEFAULT NULL,
